e2bf029c7e9511aae5fada1261a8ec21c354c35f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-08-28 12:12:50 +0100

    added button

--- javascript.js
+++ javascript.js
@@ -1,44 +1,18 @@
-let ja = false;
-let finish = false;
-let next = 0;
-const text = ["..-", "-.-", "-.-", "-.-", "-.-", "-.--"]
+function getCodes() {
 
-function progress() {
-    let output = document.getElementById("op");
-    output.innerHTML = range.value;
+    navigator.clipboard.writeText(
+        [].map.call(document.querySelectorAll("li"), function(code) {
+            return code.innerHTML;
+        }).join("\n")
+    )
+    write()
+}
 
-    if (range.value == 50 && !ja) {
-        ja = true;
-        console.log("aqui")
-        createMorse();
+function write() {
+    const main = document.querySelector("main")
+    if (main.lastChild.nodeName.toLowerCase() !== "h2") {
+        const h2 = document.createElement("h2")
+        h2.innerText = "Copiado para a área de transferência!"
+        main.appendChild(h2)
     }
-    if (ja && !finish) {
-        updateMorse();
-        console.log("primeiro aqui")
-    } 
 }
-
-function createMorse() {
-    let all = document.querySelector("#all")
-    let more = document.createElement("div")
-    more.id = "morse"
-    more.textContent = text;
-    all.appendChild(more)
-}
-
-function updateMorse() {
-    let all = document.querySelector("#morse").innerHTML
-    let tal = all + "F" + text[5];
-    next++;
-    if (text.length == next) {
-        finish = true;
-    }
-    all.textContent = tal
-}
-
-function main () {
-    const range = document.getElementById("range");
-    range.oninput = progress;
-}
-
-main()
