814884024d89f7d2ceace67b4b4156cedf480b81
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-08-28 10:59:04 +0100

    format

--- javascript.js
+++ javascript.js
@@ -1,12 +1,44 @@
-var range = document.getElementById("range");
-var output = document.getElementById("op");
-output.innerHTML = range.value;
-range.oninput = function () {
-    output.innerHTML = this.value;
-    if (range.value == 50) {
-        var all = document.querySelector("#all")
-        all.innerHTML = ""
+let ja = false;
+let finish = false;
+let next = 0;
+const text = ["..-", "-.-", "-.-", "-.-", "-.-", "-.--"]
+
+function progress() {
+    let output = document.getElementById("op");
+    output.innerHTML = range.value;
+
+    if (range.value == 50 && !ja) {
+        ja = true;
+        console.log("aqui")
+        createMorse();
     }
+    if (ja && !finish) {
+        updateMorse();
+        console.log("primeiro aqui")
+    } 
 }
 
-console.log(range.value);
+function createMorse() {
+    let all = document.querySelector("#all")
+    let more = document.createElement("div")
+    more.id = "morse"
+    more.textContent = text;
+    all.appendChild(more)
+}
+
+function updateMorse() {
+    let all = document.querySelector("#morse").innerHTML
+    let tal = all + "F" + text[5];
+    next++;
+    if (text.length == next) {
+        finish = true;
+    }
+    all.textContent = tal
+}
+
+function main () {
+    const range = document.getElementById("range");
+    range.oninput = progress;
+}
+
+main()
